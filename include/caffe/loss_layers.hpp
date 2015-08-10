178330b5fccc2fb7a9c8d1c899eb944323da3913
freesouls <declanxu@gmail.com>
2015-08-10 19:32:22 +0800

    add funcs for tripletLoss

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -765,7 +765,50 @@ class SoftmaxWithLossLayer : public LossLayer<Dtype> {
 
 template <typename Dtype>
 class TripletLossLayer : public LossLayer<Dtype> {
+public:
+    explicit TripletLossLayer(const LayerParameter& param)
+     : LossLayer<Dtype>(param) {}
+    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+     const vector<Blob<Dtype>*>& top);
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+     const vector<Blob<Dtype>*>& top);
 
+    virtual inline const char* type() const { return "TripletLoss"; }
+
+    // the return value should change, it is wrong for now
+    virtual inline int ExactNumBottomBlobs() const { return 2; }
+    virtual inline int ExactNumTopBlobs() const { return 1; }
+    virtual inline int MinTopBlobs() const { return 1; }
+    virtual inline int MaxTopBlobs() const { return 2; }
+
+protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+        const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+        const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+    /// The internal TripletLossLayer used to map predictions to a distribution.
+    shared_ptr<Layer<Dtype> > tripletloss_layer_;
+    /// prob stores the output probability predictions from the SoftmaxLayer.
+    Blob<Dtype> prob_;
+    /// bottom vector holder used in call to the underlying TripletLossLayer::Forward
+    vector<Blob<Dtype>*> tripletloss_bottom_vec_;
+    /// top vector holder used in call to the underlying TripletLossLayer::Forward
+    vector<Blob<Dtype>*> tripletloss_top_vec_;
+    /// Whether to ignore instances with a certain label.
+    bool has_ignore_label_;
+    /// The label indicating that an instance should be ignored.
+    int ignore_label_;
+
+    /// Whether to normalize the loss by the total number of values present
+    /// (otherwise just by the batch size).
+    bool normalize_;
+
+    int tripletloss_axis_, outer_num_, inner_num_;
 };
 
 }  // namespace caffe
